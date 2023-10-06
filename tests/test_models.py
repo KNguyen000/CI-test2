2189c8aa699ad90289c00e1ae26b820fc5e7fa2a
KNguyen000 <cheesy101@hotmail.com>
2023-10-06 13:12:47 +1000

    test

--- tests/test_models.py
+++ tests/test_models.py
@@ -14,7 +14,11 @@ def test_model_name():
     Model("Resnet", 0.9, 0.8)
 ])
 
-def test_modelName_str(test_input: Model):
+def test_model_validity(test_input: Model):
     assert type(test_input.name)==str
     assert test_input.accuracy < 1
     assert test_input.f1 < 1
+
+@pytest.mark.xfail
+def test_model_fail():
+    assert test_model_validity(Model("Resnet",1.2, 0.8))
